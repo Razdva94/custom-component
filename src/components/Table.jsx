719514bf200a9d0d3eee85326a2db37ab4e39ef2
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-04-22 08:12:22 +0300

    developing hooks

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,62 +3,86 @@ import TableTemplate from "react-bootstrap/Table";
 import shopsRawData from "../mock_stores.json";
 
 function Table() {
+  const [shopsData, setShopsData] = React.useState(shopsRawData);
 
-  const singleShopYearBenefit = shopsRawData.map(function (shop) {
+  // function inputBenefitByUser(event){
+
+  // }
+
+  function MonthValue(value) {
+    console.log(value);
+  }
+  MonthValue();
+  const singleShopYearBenefit = shopsData.map(function (shop) {
     return shop.months.reduce(function (sum, month) {
       return sum + month.value;
     }, 0);
   });
   const allShopsMonthBenefit = [];
   for (let i = 0; i < 12; i++) {
-    allShopsMonthBenefit[i] = shopsRawData.reduce(function (sum, shop) {
+    allShopsMonthBenefit[i] = shopsData.reduce(function (sum, shop) {
       sum = sum + shop.months[i].value;
       return sum;
     }, 0);
   }
   const totalRawBenefit = allShopsMonthBenefit.map((benifit, i) => {
     return (
-        <td key={i} className="table__size">
-          <input type="text" className="table__size" value={benifit} />
-        </td>
+      <td key={i} className="table__size">
+        <input
+          type="text"
+          className="table__size"
+          value={`за месяц ${benifit}`}
+          readOnly
+        />
+      </td>
     );
   });
-  const shops = shopsRawData.map((shop, i) => {
+  const shops = shopsData.map((shop, i) => {
     const monthsRow = shop.months.map((month) => {
       return (
-          <td key={month.id}>
+        <React.Fragment key={month.id}>
+          <MonthValue value={month.value} />
+          <td className="table__size">
             <input
               type="text"
               className="table__size"
-              value={month.value}
+              defaultValue={month.value}
+              // onChange={""}
             />
           </td>
+        </React.Fragment>
       );
     });
     return (
-        <tr key={shop.store.id}>
-          <td className="table__size table__store-name">{shop.store.name}</td>
-          {monthsRow}
-          <td>
-            <input type="text" className="table__size" value={singleShopYearBenefit[i]} />
-          </td>
-        </tr>
+      <tr key={shop.store.id} className="table__size">
+        <td className="table__size table__store-name">{shop.store.name}</td>
+        {monthsRow}
+        <td>
+          <input
+            type="text"
+            className="table__size"
+            value={`за год ${singleShopYearBenefit[i]}`}
+            readOnly
+          />
+        </td>
+      </tr>
     );
   });
   return (
-    <form>
+    <form className="body">
       <TableTemplate striped="columns" size="xl">
         <tbody>
           {shops}
           <tr>
             <td className="table__size table__store-name">Total</td>
             {totalRawBenefit}
-            <td className='table__size table__stor-name'><input type='text' className="table__size" value={''} /></td>
+            <td className="table__size table__stor-name">
+              <input type="text" className="table__size" value={"а"} readOnly />
+            </td>
           </tr>
         </tbody>
       </TableTemplate>
     </form>
   );
 }
-
 export default Table;
